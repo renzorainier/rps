f12bcdeef1145df8d3d19a66e4859744ee0345d5
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-07-10 08:35:35 +0000

    Mon, Jul 10, 2023, 8:35 AM +00:00

--- app/Play.jsx
+++ app/Play.jsx
@@ -6,6 +6,7 @@ const Play = () => {
   const [selectedOption, setSelectedOption] = useState(null);
   const [player2Option, setPlayer2Option] = useState(null);
   const [player2State, setPlayer2State] = useState(null);
+  const [gameResult, setGameResult] = useState(null);
 
   const handleOptionClick = async (option) => {
     setSelectedOption(option);
@@ -23,13 +24,34 @@ const Play = () => {
         const data = doc.data();
         setPlayer2Option(data.p2);
         setPlayer2State(data.p2state);
+
+        // Compare the options to determine the winner
+        if (selectedOption && data.p2) {
+          const winner = determineWinner(selectedOption, data.p2);
+          setGameResult(winner);
+        }
       }
     });
 
     return () => {
       unsubscribe();
     };
-  }, []);
+  }, [selectedOption]);
+
+  const determineWinner = (option1, option2) => {
+    // Rock beats scissors, scissors beat paper, paper beats rock
+    if (option1 === option2) {
+      return 'Draw';
+    } else if (
+      (option1 === 'rock' && option2 === 'scissors') ||
+      (option1 === 'scissors' && option2 === 'paper') ||
+      (option1 === 'paper' && option2 === 'rock')
+    ) {
+      return 'Player 1 wins!';
+    } else {
+      return 'Player 2 wins!';
+    }
+  };
 
   return (
     <div className="flex flex-col justify-center items-center h-screen">
@@ -37,6 +59,7 @@ const Play = () => {
       <div className="mb-4 text-lg">
         Player 2: {player2Option} {player2State}
       </div>
+      {gameResult && <div className="mb-4 text-lg">{gameResult}</div>}
       <div className="space-x-4">
         {['rock', 'paper', 'scissors'].map((option) => (
           <button
@@ -50,6 +73,6 @@ const Play = () => {
       </div>
     </div>
   );
-  };
+};
 
 export default Play;
