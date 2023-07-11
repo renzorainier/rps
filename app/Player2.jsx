40065c4aad93f4952b13809c03afec3bc18a87cf
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-07-11 13:31:37 +0000

    Tue, Jul 11, 2023, 1:31 PM +00:00

--- app/Player2.jsx
+++ app/Player2.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { collection, doc, updateDoc, onSnapshot } from "firebase/firestore";
-import { db } from "./firebase.js";
 
-const Play = () => {
+const Player2 = () => {
   const [selectedOption, setSelectedOption] = useState(null);
   const [player2Option, setPlayer2Option] = useState(null);
   const [player2State, setPlayer2State] = useState(null);
@@ -19,54 +18,6 @@ const Play = () => {
     }
   };
 
-  const handleResetGame = async () => {
-    setSelectedOption(null);
-    setPlayer2Option(null);
-    setPlayer2State(null);
-    setDisabled(false);
-
-    const player1DocRef = doc(db, "rps", "player1");
-    await updateDoc(player1DocRef, { p1: "", p1state: "" });
-
-    const player2DocRef = doc(db, "rps", "player2");
-    await updateDoc(player2DocRef, { p2: "", p2state: "" });
-  };
-
-  useEffect(() => {
-    const player2DocRef = doc(db, "rps", "player2");
-    const unsubscribe = onSnapshot(player2DocRef, (doc) => {
-      if (doc.exists()) {
-        const data = doc.data();
-        setPlayer2Option(data.p2);
-        setPlayer2State(data.p2state);
-
-        if (selectedOption && data.p2) {
-          const winner = determineWinner(selectedOption, data.p2);
-          setGameResult(winner);
-          setDisabled(false);
-          handleResetGame();
-        }
-      }
-    });
-
-    return () => {
-      unsubscribe();
-    };
-  }, [selectedOption]);
-
-  const determineWinner = (option1, option2) => {
-    if (option1 === option2) {
-      return "Draw";
-    } else if (
-      (option1 === "rock" && option2 === "scissors") ||
-      (option1 === "scissors" && option2 === "paper") ||
-      (option1 === "paper" && option2 === "rock")
-    ) {
-      return "Player 1 wins!";
-    } else {
-      return "Player 2 wins!";
-    }
-  };
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
@@ -90,4 +41,4 @@ const Play = () => {
   );
 };
 
-export default Play;
+export default Player2;
