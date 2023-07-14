27b86a2f180612c4193e788f34b3be127616bd31
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-07-14 11:47:50 +0000

    Fri, Jul 14, 2023, 11:47 AM +00:00

--- app/Player2.jsx
+++ app/Player2.jsx
@@ -4,7 +4,6 @@ import { db } from "./firebase.js";
 
 const Player2 = () => {
   const [selectedOption, setSelectedOption] = useState(null);
-  const [player2Option, setPlayer2Option] = useState(null);
   const [gameResult, setGameResult] = useState(null);
   const [disabled, setDisabled] = useState(false);
   const [previousResults, setPreviousResults] = useState([]);
@@ -25,54 +24,8 @@ const Player2 = () => {
     await updateGameState({ p2: "", winner: "" });
   };
 
-  useEffect(() => {
-    const player2DocRef = doc(db, "rps", "state");
-    const unsubscribe = onSnapshot(player2DocRef, (doc) => {
-      const data = doc.data();
-      setPlayer2Option(data.p2);
 
-      if (selectedOption && data.p2 && !gameResult) {
-        const winner = determineWinner(selectedOption, data.p2);
-        setGameResult(winner);
-        handleUpdateWinner(winner);
-      }
-    });
 
-    return () => {
-      unsubscribe();
-    };
-  }, [selectedOption]);
-
-  useEffect(() => {
-    if (gameResult) {
-      setPreviousResults((prevResults) => [...prevResults, gameResult]);
-      setDisabled(false); // Enable buttons after game result
-      setTimeout(handleResetGame, 500);
-    }
-  }, [gameResult]);
-
-  const determineWinner = (option1, option2) => {
-    if (option1 === option2) {
-      return "Draw";
-    } else if (
-      (option1 === "rock" && option2 === "scissors") ||
-      (option1 === "scissors" && option2 === "paper") ||
-      (option1 === "paper" && option2 === "rock")
-    ) {
-      return "Player 1 wins!";
-    } else {
-      return "Player 2 wins!";
-    }
-  };
-
-  const handleUpdateWinner = async (winner) => {
-    await updateGameState({ winner });
-  };
-
-  const updateGameState = async (data) => {
-    const player2DocRef = doc(db, "rps", "state");
-    await updateDoc(player2DocRef, data);
-  };
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
