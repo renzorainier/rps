48b23a471bc17c8386a86fc1a0b7d7c6cd35a1c5
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-08-01 07:08:49 +0000

    Tue, Aug 1, 2023, 7:08 AM +00:00

--- app/Main.jsx
+++ app/Main.jsx
@@ -1,21 +1,45 @@
-'use client'
 
-import React, { useState, useEffect } from "react";
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "./firebase.js";
-import Play from "./Play.jsx"
-import Player2 from "./Player2.jsx"
+import React, { useState } from "react";
+import Play from "./Play.jsx";
+import Player2 from "./Player2.jsx";
 
+const Main = () => {
+  const [showMain, setShowMain] = useState(true);
+  const [showPlayer1, setShowPlayer1] = useState(false);
+  const [showPlayer2, setShowPlayer2] = useState(false);
 
-function Main() {
+  const handlePlayer1ButtonClick = () => {
+    setShowMain(false);
+    setShowPlayer1(true);
+  };
+
+  const handlePlayer2ButtonClick = () => {
+    setShowMain(false);
+    setShowPlayer2(true);
+  };
+
   return (
-    <div>
-
-      <Play/>
-      <Player2/>
-
-    </div>
-  )
-}
+    <>
+      {showMain && (
+        <div className="flex flex-col items-center justify-center h-screen">
+          <button
+            onClick={handlePlayer1ButtonClick}
+            className="px-4 py-2 mb-4 bg-blue-500 text-white rounded hover:bg-blue-600"
+          >
+            Player 1
+          </button>
+          <button
+            onClick={handlePlayer2ButtonClick}
+            className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+          >
+            Player 2
+          </button>
+        </div>
+      )}
+      {showPlayer1 && <Play />}
+      {showPlayer2 && <Player2 />}
+    </>
+  );
+};
 
 export default Main;
